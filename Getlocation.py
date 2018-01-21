280715eeca878e22a02fcd2ec9d2becfefff5028
Sh3mm <lesingeamoi@gmail.com>
2018-01-21 09:18:40 -0500

    fixed some bug in Getlocation

--- Getlocation.py
+++ Getlocation.py
@@ -21,7 +21,7 @@ lat = []
 for x in range(len(json_data['latitude']))[1::]:
     if float(json_data['latitude'][x]) < latitude + (float(1)/111) and float(json_data['latitude'][x]) > latitude - (float(1) / 111):
         if float(json_data['longitude'][x]) < longitude + (float(1) / 111) and float(json_data['longitude'][x]) > longitude - (float(1) / 111):
-            print(float(json_data['latitude'][x]), longitude + (float(1) / 111), (float(1) / 111))
+
             lon += [json_data['longitude'][x]]
             lat+= [json_data['latitude'][x]]
 
@@ -29,4 +29,4 @@ output = {'longitude': lon, 'latitude': lat}
 
 with open('map.json', 'w') as f:
     json.dump(output, f)
-print(len(lon))
+print(1)
