792b117c5f617bd5d6d010c8cc4aa58097c8f0e1
Sh3mm <lesingeamoi@gmail.com>
2018-01-20 22:28:43 -0500

    added the proximity algorithm

---
+++ Getlocation.py
@@ -0,0 +1,30 @@
+import json
+from sys import argv
+
+def getopts(input):
+    dic = {}
+    while input:
+        if input[0][0] == '-':
+            dic[input[0]] = input[1]
+        input = input[1:]
+    return dic
+
+with open("data.json") as json_file:
+    json_data = json.load(json_file)
+
+longitude = float(argv[1])
+
+latitude = float(argv[2])
+lon = []
+lat = []
+
+for x in range(len(json_data['latitude']))[1::]:
+    if float(json_data['latitude'][x]) < latitude +(1/111) and float(json_data['longitude'][x]) < longitude +(1/111):
+        lon += [json_data['longitude'][x]]
+        lat += [json_data['latitude'][x]]
+
+output = {'longitude': lon, 'latitude': lat}
+
+with open('map.json', 'w') as f:
+    json.dump(output, f)
+print(1)
